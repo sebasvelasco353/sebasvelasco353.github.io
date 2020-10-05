baca5f6388a915770c1cf24a64a4324d7589065b
Sebastian Velasco <sebastianvelasco@ZCOCLMH101338-SebastianVelasco.local>
2020-10-05 17:27:10 -0500

    micro interactions

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -2,13 +2,13 @@
   <nav>
       <span class="nav_logo">Velas_Code</span>
       <ul>
-          <li><nuxt-link to="/">Home</nuxt-link></li>
-          <li><nuxt-link to="/about">About</nuxt-link></li>
+          <li><nuxt-link class="nav_link" to="/">Home</nuxt-link></li>
+          <li><nuxt-link class="nav_link" to="/about">About</nuxt-link></li>
       </ul>
       <ul class="nav_social">
-          <li><a href="https://www.instagram.com/velas_code">Instagram</a></li>
-          <li><a href="https://www.github.com/sebasvelasco353">Github</a></li>
-          <li><a href="https://www.youtube.com/channel/UC1H83UvGm0AznLAZwsYPoNA">Youtube</a></li>
+          <li><a class="nav_link" href="https://www.instagram.com/velas_code">Instagram</a></li>
+          <li><a class="nav_link" href="https://www.github.com/sebasvelasco353">Github</a></li>
+          <li><a class="nav_link" href="https://www.youtube.com/channel/UC1H83UvGm0AznLAZwsYPoNA">Youtube</a></li>
       </ul>
   </nav>
 </template>
@@ -42,12 +42,27 @@ export default {}
                 list-style-type:none;
                 a{
                     color: black;
+                    text-decoration: none;
+                    display: inline-block;
                 }
             }
         }
+        .nav_link::after{
+            content: '';
+            display: block;
+            width: 0;
+            height: 2px;
+            background-color: black;
+            transition: width .3s;
+        }
+        .nav_link:hover::after{
+            width: 100%;
+            transition: width;
+            transition: width .3s;
+        }
         .nuxt-link-exact-active{
             font-weight: bold;
-            text-decoration: underline;
         }
     }
+
 </style>
