f0fe06bf66cc336e055be9cbf7aca6071b33c4db
Sebastian Velasco <sebastianvelasco@ZCOCLMH101338-SebastianVelasco.local>
2020-10-05 12:05:17 -0500

    first commit

---
+++ components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav>
+      <span class="nav_logo">Velas_Code</span>
+      <ul>
+          <li>About</li>
+          <li>Blog</li>
+      </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
