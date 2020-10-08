44e1552d77dd4345b477c277b1ece00f88426da0
Sebastian Velasco <sebasvelasco353@hotmail.com>
2020-10-07 19:28:59 -0500

    played around with the design anded up doing not much but cool

--- pages/blog_index.vue
+++ pages/blog_index.vue
@@ -3,17 +3,17 @@
     <h1>Blog Posts</h1>
     <ul class="post_link-container">
       <li class="post_link" v-for="article in articles" :key="article.slug">
-          <nuxt-link class="nav_link" :to="`blog/${article.slug}`" exact>
-            <div class="post_img">
-              <img
-                :src="require(`~/assets/images/${article.img}`)"
-                :alt="article.alt"
-              />
-            </div>
-            <h3>{{ article.title }}</h3>
-            <p>{{ article.description }}</p>
-            <p><b>By: </b>{{ article.author }}</p>
-          </nuxt-link>
+        <nuxt-link class="nav_link" :to="`blog/${article.slug}`" exact>
+          <div class="post_img">
+            <img
+              :src="require(`~/assets/images/${article.img}`)"
+              :alt="article.alt"
+            />
+          </div>
+          <h3>{{ article.title }}</h3>
+          <p>{{ article.description }}</p>
+          <p><b>By: </b>{{ article.author }}</p>
+        </nuxt-link>
       </li>
     </ul>
   </Container>
@@ -26,7 +26,6 @@ export default {
       .only(['title', 'description', 'img', 'slug', 'createdAt', 'author'])
       .sortBy('createdAt', 'asc')
       .fetch()
-    console.log(articles)
     return {
       articles
     }
@@ -40,10 +39,14 @@ export default {
   flex-direction: row;
   list-style-type: none;
   flex-wrap: wrap;
+  justify-content: space-between;
+  padding: 0;
   .post_link{
-    margin: 40px 10px;
+    margin: 20px 0px;
     box-sizing: border-box;
-    max-width: 35vw;
+    width: 35vw;
+    height:30vh;
+    padding: 20px;
     a{
       color: black;
       text-align: left;
@@ -51,7 +54,6 @@ export default {
       .post_img{
         width: 100%;
         height: 200px;
-        background-color: blue;
         overflow: hidden;
         img{
           width: 100%;
