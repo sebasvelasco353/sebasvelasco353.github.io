83573ae63e4e56d95660938eaeae760d47780e0b
Sebastian Velasco <11669998+sebasvelasco353@users.noreply.github.com>
2021-05-22 00:38:13 -0500

    navigation menu has animation

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,14 +1,25 @@
 <template>
-    <nav class="navbar">
+<header>
+    <div class="navbar">
         <h1>velas<span>code_</span></h1>
         <div class="navbar__button" @click="changeMenuVisibility()">
             <span>{{ menuText }}</span>
         </div>
-    </nav> 
+    </div>
+    <nav class="navbar__links">
+        <ul>
+            <li class="navbar__link">Home</li>
+            <li class="navbar__link">About me</li>
+            <li class="navbar__link">Work</li>
+            <li class="navbar__link">Blog</li>
+        </ul>
+    </nav>
+</header>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
+import gsap from "gsap";
 import { Navbar } from '../types/navbarTypes';
 
 export default Vue.extend({
@@ -20,16 +31,40 @@ export default Vue.extend({
     },
     methods: {
         changeMenuVisibility(): void {
-            this.menuText === 'open' ? this.menuText = 'close' : this.menuText = 'open';
+            if (this.menuText === 'open') {
+                this.menuText = 'close';
+                gsap.to('.navbar__links', {
+                    duration: 1,
+                    ease: 'power2.out',
+                    yPercent: 120
+                });
+            } else {
+                this.menuText = 'open';
+                gsap.to('.navbar__links', {
+                    duration: 1,
+                    ease: 'power2.out',
+                    yPercent: -120
+                });
+            }
         }
+    },
+    mounted(): void {
     }
 })
 </script>
+
+<style lang="postcss">
+body {
+    @apply overflow-hidden;
+}
+</style>
 
 <style lang="postcss" scoped>
 .navbar {
     /* sizing */
     @apply w-full h-16 px-4;
+    /* positioning */
+    @apply absolute top-0 left-0 z-50;
     /* shadow */
     @apply shadow;
     /* Colors */
@@ -43,10 +78,17 @@ h1 {
 h1 > span {
     @apply font-light;
 }
-.nav__button {
-    /* sizing */
-    @apply w-8 h-1;
-    /* color */
-    @apply bg-orange;
+.navbar__button {
+}
+.navbar__links {
+    /* position */
+    transform: translateY(-120%);
+    @apply absolute top-0 left-0 z-40;
+    /* Sizing */
+    @apply w-full h-screen;
+    /* Colors */
+    @apply bg-black;
+    /* display */
+    @apply flex content-center justify-center items-center;
 }
 </style>
